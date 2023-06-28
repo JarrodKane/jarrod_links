b3bc37c22227f4f02905eec814dee662ac0abe5c
Jarrod <kajarrod@gmail.com>
2023-06-28 18:58:07 +1000

    basic site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,31 @@
-import Image from 'next/image'
-import { Link } from '../components/link'
+import Image from 'next/image';
+import { Link } from '../components/link';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 sm:p-24">
-
-
-
-
-      <div className='flex flex-col gap-6 w-full'>
-        <Image src="/jarrod.jpeg" className="rounded-full w-48 h-48" width={150} height={150} alt='Jarrod doing MMA' />
-        <Link href="https://www.instagram.com/jarrod_kane/?igshid=ZDc4ODBmNjlmNQ%3D%3D">Instagram</Link>
+      <div className="flex flex-col gap-6 w-full items-center ">
+        <Image
+          src="/jarrod.jpeg"
+          className="rounded-full w-48 h-48  ring-2 ring-gray-300 "
+          width={150}
+          height={150}
+          alt="Jarrod doing MMA"
+        />
+        <Link href="https://www.instagram.com/jarrod_kane/?igshid=ZDc4ODBmNjlmNQ%3D%3D">
+          Instagram
+        </Link>
         <Link href="https://www.tiktok.com/@jarrod_mma">MMA Tiktok</Link>
-        <Link href="https://www.youtube.com/channel/UCGvSXLPI1gLiwSVi-HeZBwQ">MMA Youtube</Link>
-        <Link href="https://www.youtube.com/channel/UCFvq3tK6QXfqksQ_HQljN3g">Movies Youtube</Link>
-        <Link href="https://www.youtube.com/channel/UCwBJ5gLp3trHUDtDjZvQH2Q">Tech Youtube</Link>
+        <Link href="https://www.youtube.com/channel/UCGvSXLPI1gLiwSVi-HeZBwQ">
+          MMA Youtube
+        </Link>
+        <Link href="https://www.youtube.com/channel/UCFvq3tK6QXfqksQ_HQljN3g">
+          Movies Youtube
+        </Link>
+        <Link href="https://www.youtube.com/channel/UCwBJ5gLp3trHUDtDjZvQH2Q">
+          Tech Youtube
+        </Link>
       </div>
     </main>
-  )
+  );
 }
--- src/components/link.tsx
+++ src/components/link.tsx
@@ -7,11 +7,7 @@ export const Link: React.FC<LinkProps> = ({ href, children }) => {
   const linkStyle = `inline-block px-4 py-4 bg-white text-gray-900 font-bold rounded-md transition-all   
   w-full  hover:translate-x-1	hover:translate-y-1	shadow-comic hover:shadow-comic-h text-center	 `;
   return (
-    <a
-      target="_blank"
-      href={href}
-      className={linkStyle}
-    >
+    <a target="_blank" href={href} className={linkStyle}>
       {children}
     </a>
   );
