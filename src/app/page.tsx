c6c5c334d7aef17b29985588ced4310d9b15b722
Jarrod <kajarrod@gmail.com>
2023-07-01 21:21:40 +1000

    Added in data.json and added in icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,43 @@
 import Image from 'next/image';
+import data from '../../data.json';
 import { Link } from '../components/link';
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 sm:p-24">
-      <div className="flex flex-col gap-6 w-full items-center max-w-lg		">
+      <div className="flex flex-col gap-10 w-full items-center max-w-lg		">
         <Image
           src="/jarrod.jpeg"
           className="rounded-full w-48 h-48  ring-2 ring-gray-300 drop-shadow-md"
           width={150}
           height={150}
-          alt="Jarrod doing MMA"
+          alt={data.name}
         />
-        <Link href="https://www.instagram.com/jarrod_kane/?igshid=ZDc4ODBmNjlmNQ%3D%3D">
-          Instagram
-        </Link>
-        <Link href="https://www.tiktok.com/@jarrod_mma">MMA Tiktok</Link>
-        <Link href="https://www.youtube.com/channel/UCGvSXLPI1gLiwSVi-HeZBwQ">
-          MMA Youtube
-        </Link>
-        <Link href="https://www.youtube.com/channel/UCFvq3tK6QXfqksQ_HQljN3g">
-          Movies Youtube
-        </Link>
-        <Link href="https://www.youtube.com/channel/UCwBJ5gLp3trHUDtDjZvQH2Q">
-          Tech Youtube
-        </Link>
+        <div className='flex flex-col gap-6	w-full'>
+          {data.links.map((link) => (
+            <Link key={link.href} href={link.href}>
+              {link.name}
+            </Link>
+          ))}
+        </div>
+
+        <div className="flex flex-row w-full items-center max-w-lg justify-center	gap-3">
+          {data.socials.map((icon) => (
+            <a key={icon.href} target="_blank" href={icon.href} >
+              <Image
+                src={`/${icon.name}.svg`}
+                className="rounded-full w-16 h-16 ring-2 ring-gray-300 drop-shadow-md 
+                 hover:ring-4 hover:scale-110 transition-all"
+                width={50}
+                height={50}
+                alt={icon.name}
+              />
+            </a>
+          ))}
+        </div>
       </div>
+
     </main>
   );
 }
