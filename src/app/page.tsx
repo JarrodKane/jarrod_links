d19af50966110adcd8601caf76dddd6eea3cdfde
JarrodKane <kajarrod@gmail.com>
2023-11-06 23:44:47 +1100

    Added in some more links and minor css changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,22 @@ import Image from 'next/image';
 import data from '../../data.json';
 import { Link } from '../components/link';
 
+
 export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 sm:p-24">
       <div className="flex flex-col gap-10 w-full items-center max-w-lg		">
         <Image
-          src="/jarrod.jpeg"
-          className="rounded-full w-48 h-48  ring-2 ring-gray-300 drop-shadow-md"
-          width={150}
-          height={150}
+          src="/jarrod.png"
+          className="rounded-xl w-52 h-52  ring-2 ring-gray-300 drop-shadow-md"
+          width={250}
+          height={250}
           alt={data.name}
         />
         <div className='flex flex-col gap-6	w-full'>
           {data.links.map((link) => (
-            <Link key={link.href} href={link.href}>
+            <Link key={link.href} href={link.href} icon={link.icon}>
               {link.name}
             </Link>
           ))}
--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,13 +1,37 @@
+import { FaInstagram, FaTiktok, FaYoutube } from 'react-icons/fa';
+import { SiOnlyfans } from 'react-icons/Si';
+
 type LinkProps = {
   href: string;
   children: React.ReactNode;
+  icon: string;
 };
 
-export const Link: React.FC<LinkProps> = ({ href, children }) => {
-  const linkStyle = `inline-block px-4 py-4 bg-white text-gray-900 font-bold rounded-md transition-all   
-  w-full  hover:translate-x-1	hover:translate-y-1	shadow-comic hover:shadow-comic-h text-center	 `;
+export const Link: React.FC<LinkProps> = ({ href, icon, children }) => {
+  const linkStyle = `flex justify-center px-4 py-4 bg-white text-gray-900 font-bold rounded-md transition-all   
+  w-full  hover:translate-x-1	hover:translate-y-1	shadow-comic hover:shadow-comic-h text-center	align-middle items-center	 gap-3  `;
+
+
+
+  const iconElement = () => {
+    if (icon === 'youtube') {
+      return <FaYoutube />
+    } else if (icon === 'instagram') {
+      return <FaInstagram />
+    } else if (icon === 'onlyfans') {
+      return <SiOnlyfans />
+    } else if (icon === 'tiktok') {
+      return <FaTiktok />
+    } else {
+      return <FaYoutube />
+    }
+  }
+
+  const Icon = iconElement();
+
   return (
     <a target="_blank" href={href} className={linkStyle}>
+      {Icon}
       {children}
     </a>
   );
